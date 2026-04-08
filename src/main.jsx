0fff85288a2b42aa88a67f61db4375fe60df57d7
ShreeyaChavan <151952281+ShreeyaChavan@users.noreply.github.com>
2026-04-08 21:53:05 +0530

    Integrate AWS Amplify and AuthProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
-//main
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { team2023, team2024 } from "./constants";
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { AuthProvider } from './context/AuthContext';
 import App from "./App.jsx";
 import "./index.css";
-import Team from "./components/Team.jsx"; 
+import Team from "./components/Team.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Register from "./components/Register.jsx";
 import Events from "./components/Events.jsx";
 import Blog from "./components/Blog.jsx";
-import Winners from "./components/Winners";
+import Winners from "./components/Winners.jsx";
+import Login from "./components/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
+// Configure Amplify
+Amplify.configure(awsConfig);
 
 const router = createBrowserRouter([
   {
@@ -24,32 +30,41 @@ const router = createBrowserRouter([
     element: (
       <div className="bg-black text-white min-h-screen pt-[5rem]">
         <Header />
-        <Team />  
+        <Team />
         <Footer />
       </div>
     ),
   },
   {
     path: "/register",
-    element: <Register />
+    element: <Register />,
   },
   {
     path: "/events",
-    element: <Events />
+    element: <Events />,
   },
   {
     path: "/blogs",
-    element: <Blog />
+    element: <Blog />,
   },
   {
-  path: "/winners",
-  element: <Winners />
-},
-
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/winners",
+    element: (
+      <ProtectedRoute>
+        <Winners />
+      </ProtectedRoute>
+    ),
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
