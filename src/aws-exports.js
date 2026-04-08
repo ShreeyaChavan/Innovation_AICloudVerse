733a9708a333a5a8abc556c2572be901eb294a87
ShreeyaChavan <151952281+ShreeyaChavan@users.noreply.github.com>
2026-04-08 23:16:25 +0530

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,7 +2,7 @@ const awsConfig = {
   Auth: {
     Cognito: {
       userPoolId: 'ap-south-1_jj3URifz7',
-      userPoolClientId: '6mbclsppc44vg0379vkia3115n',
+      userPoolClientId: '6mbclsppc44vgo379vkia3115n',
       region: 'ap-south-1',
       loginWith: {
         username: true,    // allow username/password login
